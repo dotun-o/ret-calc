d2e1dac3ea82e91a087aed65aa4eacf4985eef5b
Dotun Ogunsakin <email@dotun.me>
2017-06-20 17:41:50 -0400

    Fields added & styled.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<script src="scripts/main.js"></script>
+
+<link href="styles/main.css" rel="stylesheet">
+
+<title> 401k Retirement Calculator</title>
+<head>
+</head>
+
+<body>
+<form id="ret-calc-form">
+<label for="">Annual Contribution (%)</label>
+<label for="">Annual Salary ($)</label>
+<label for="">Annual Salary Increase (%)</label>
+<label for="">Current Age</label>
+<label for="">Retirement Age</label>
+<label for="">Current 401k Balance ($)</label>
+<label for="">Annual Rate of Return (%)</label>
+<label for="">Employer Match (%)</label>
+
+
+<label for="">Enter Your Current 401k Balance:</label>
+<label for="">Enter Total monthly Contributions (yours, employer match, catch-up):</label>
+<label for="">Enter Estimated Annual Return on Investment:</label>
+<label for="">Enter Current Age:</label>
+<label for="">Enter Retirement Age:</label>
+</form>
+</body>
+</html>
---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+(function Calculator()
+{
+    if(!window.Calculator)
+    {
+        window.Calculator = {
+            current: 0,
+            contributions: 500,
+            roi: 7,
+            years: 30
+        };
+    }
+}());
